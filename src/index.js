56559c5e92a33e62934b774d103c56d696d0dcd0
alexnikrod <rodkin.alex1@gmail.com>
2020-03-02 09:16:57 +0100

    copying state in reducers

--- src/index.js
+++ src/index.js
@@ -6,19 +6,11 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-let renderEntireTree = () => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>,
-    document.getElementById("root")
-  );
-};
-
-renderEntireTree();
-
-store.subscribe(() => {
-  renderEntireTree();
-});
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
