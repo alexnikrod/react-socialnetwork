42501c9cc5db1b022fbfae70ab1f0517baba16b7
alexnikrod <rodkin.alex1@gmail.com>
2020-03-04 12:20:12 +0100

    users conatiner added, users is pure component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,54 @@
+import React from "react";
 import { connect } from "react-redux";
+import * as axios from "axios";
 
-import Users from "./Users";
-import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from "../../redux/users-reducer";
+import {
+  followAC,
+  unfollowAC,
+  setUsersAC,
+  setCurrentPageAC,
+  setTotalUsersCountAC
+} from "../../redux/users-reducer";
+
+import Users from './Users'
+
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then(response => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
+  }
+
+  onPageChanged = pageNumber => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then(response => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+
+  render() {
+    return (
+      <Users
+        totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage}
+        users={this.props.users}
+        follow={this.props.follow}
+        unfollow={this.props.unfollow}
+        onPageChanged={this.onPageChanged}
+      />
+    );
+  }
+}
 
 let mapStateToProps = state => {
   return {
@@ -28,8 +75,8 @@ let mapDispatchToProps = dispatch => {
     },
     setTotalUsersCount: totalUsersCount => {
       dispatch(setTotalUsersCountAC(totalUsersCount));
-    } 
+    }
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
