581b62b4ab0780eb6725be2e8ca13b2e9f267ff8
alexnikrod <rodkin.alex1@gmail.com>
2020-03-19 09:35:17 +0100

    componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -24,6 +24,15 @@ class ProfileStatus extends React.Component {
       status: e.currentTarget.value
     });
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({
+        status: this.props.status
+      });
+    }
+  }
+
   // input status
   render() {
     return (
