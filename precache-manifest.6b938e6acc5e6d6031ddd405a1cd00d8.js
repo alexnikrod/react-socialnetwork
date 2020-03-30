035cedc995d5b302dc0f333e63ec2c911d4fd055
alexnikrod <rodkin.alex1@gmail.com>
2020-03-30 11:02:09 +0200

    Updates

---
+++ precache-manifest.6b938e6acc5e6d6031ddd405a1cd00d8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab533f18210d4fc7764cece5bcf46829",
+    "url": "/react-socialnetwork/index.html"
+  },
+  {
+    "revision": "1c166aa051922596506a",
+    "url": "/react-socialnetwork/static/css/3.85d280f4.chunk.css"
+  },
+  {
+    "revision": "323d598aee944b98a363",
+    "url": "/react-socialnetwork/static/css/4.f4b21433.chunk.css"
+  },
+  {
+    "revision": "94666f27be6c11b60a78",
+    "url": "/react-socialnetwork/static/css/5.9f89b520.chunk.css"
+  },
+  {
+    "revision": "5006c225e08b2db7db74",
+    "url": "/react-socialnetwork/static/css/main.0783699e.chunk.css"
+  },
+  {
+    "revision": "6c3322a3e69a46e4240d",
+    "url": "/react-socialnetwork/static/js/2.2b41526b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-socialnetwork/static/js/2.2b41526b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c166aa051922596506a",
+    "url": "/react-socialnetwork/static/js/3.f1de9669.chunk.js"
+  },
+  {
+    "revision": "323d598aee944b98a363",
+    "url": "/react-socialnetwork/static/js/4.9f373446.chunk.js"
+  },
+  {
+    "revision": "94666f27be6c11b60a78",
+    "url": "/react-socialnetwork/static/js/5.92b84df4.chunk.js"
+  },
+  {
+    "revision": "5006c225e08b2db7db74",
+    "url": "/react-socialnetwork/static/js/main.43a659fa.chunk.js"
+  },
+  {
+    "revision": "502ab86020662c1dfaec",
+    "url": "/react-socialnetwork/static/js/runtime-main.f02c2279.js"
+  },
+  {
+    "revision": "8ae78a7ea302c54f2b6625a58fbd5002",
+    "url": "/react-socialnetwork/static/media/35.8ae78a7e.gif"
+  },
+  {
+    "revision": "9bea4bf7e48eb6f5a0a13767a58518df",
+    "url": "/react-socialnetwork/static/media/bill.9bea4bf7.jpg"
+  }
+]);
